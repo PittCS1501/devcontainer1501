09fee6bc2db6dcbf1dec4baf0dc1c99e0e7e161a
Nick Farnan <nlf4@pitt.edu>
2022-02-01 22:18:22 -0500

    Fixed commit message typo

--- make_devcontainer.sh
+++ make_devcontainer.sh
@@ -6,6 +6,6 @@ cd .devcontainer
 curl --proto '=https' --tlsv1.2 -sSfO https://raw.githubusercontent.com/PittCS1501/devcontainer1501/main/.devcontainer/Dockerfile
 curl --proto '=https' --tlsv1.2 -sSfO https://raw.githubusercontent.com/PittCS1501/devcontainer1501/main/.devcontainer/devcontainer.json
 git add .
-git commit -m "Adding .devcontainder"
+git commit -m "Adding .devcontainer"
 git push
 cd -
